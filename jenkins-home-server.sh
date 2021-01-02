41589ca0b85531554bd088f5aef68932bdbac9c0
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2021-01-02 18:05:17 -0500

    added new files names + jeknins file + new docker compose file

---
+++ jenkins-home-server.sh
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('build image push image to Docker Hub CI') {
+            steps {
+                git branch: 'main',
+                credentialsId: 'git-credentials',
+                url: 'https://github.com/progdev8201/cooking-app-backend.git'
+
+                sh 'docker system prune -a -f'
+
+                sh "docker login --username orewaprogdev13 --password kulotte2015"
+
+                sh "docker build -t orewaprogdev13/cooking-api ."
+
+                sh "docker push orewaprogdev13/cooking-api"
+            }
+        }
+
+        stage('deploy app CD') {
+            steps {
+                git branch: 'main',
+                credentialsId: 'git-credentials',
+                url: 'https://github.com/progdev8201/cooking-app-backend.git'
+
+                sh("""
+                docker-compose -f docker-compose-home-server.yml down;
+                docker system prune -a -f;
+                docker-compose -f docker-compose-home-server.yml up -d;
+                """)
+           }
+        }
+    }
+}
